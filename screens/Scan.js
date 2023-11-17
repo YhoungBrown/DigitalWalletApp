83ac5132366b29c6b96e80d0b216e48a187cbbb4
Omotola <omotolaodumosu04@gmail.com>
2023-11-17 16:43:50 +0100

    project finished

--- screens/Scan.js
+++ screens/Scan.js
@@ -3,10 +3,11 @@ import { Camera } from 'expo-camera';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import { Text, TouchableOpacity, View } from 'react-native';
 import { useFocusEffect } from '@react-navigation/native';
-import { COLORS, SIZES } from '../constants';
+import { COLORS, SIZES, FONTS, icons, Images, images } from '../constants';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Image } from 'react-native';
 
-const Scan = () => {
+const Scan = ({navigation}) => {
   const [type, setType] = useState(Camera.Constants.Type.back);
   const [hasPermission, setHasPermission] = useState(null);
   const [isFocused, setIsFocused] = useState(false); // Track whether the component is focused
@@ -52,6 +53,169 @@ const Scan = () => {
     );
   }
 
+
+  function renderHeader() {
+    return(
+      <View style={{
+        flexDirection: 'row',
+        marginTop: SIZES.padding * 4,
+        paddingHorizontal: SIZES.padding * 3,
+      }}>
+          <TouchableOpacity
+          style={{
+            width: 45,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+          onPress={() => navigation.navigate("Home")}>
+              <Image source={icons.close}
+                style={{
+                  height: 20,
+                  width: 20,
+                  tintColor: COLORS.white
+                }}
+              />
+          </TouchableOpacity>
+
+          <View 
+            style={{
+              flex: 1, 
+              alignItems: 'center', 
+              justifyContent: 'center',}}>
+            <Text style={{color: COLORS.white}}>
+            Scan For Payment</Text>
+          </View>
+
+          <TouchableOpacity style={{
+            height: 45,
+            width: 45,
+            backgroundColor: COLORS.green,
+            borderRadius: 10,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+          onPress={() => console.log("Info")}>
+              <Image 
+                source={icons.info}
+                style={{
+                  height: 25,
+                  width: 25,
+                  tintColor: COLORS.white
+                }}
+              />
+          </TouchableOpacity>
+      </View>
+    )
+  }
+
+
+  function renderScanFocus() {
+    return(
+      <View style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+          <Image
+          source={images.focus}
+          resizeMode= "stretch"
+          style={{
+            width: 200,
+            height: 300,
+            marginTop: -250,
+          }}
+          />
+      </View>
+    )
+  }
+
+
+  function renderPaymentMethods() {
+    return(
+      <View style={{
+        position: 'absolute',
+        bottom: 0,
+        left: 0, 
+        right: 0,
+        height: 220,
+        padding: SIZES.padding * 3,
+        borderTopLeftRadius: SIZES.radius,
+        borderTopRightRadius: SIZES.radius,
+        backgroundColor: COLORS.white,
+      }}>
+          <Text style={{...FONTS.h4}}>
+            Another Payment Method
+          </Text>
+          <View
+            style={{
+              flex: 1, 
+              flexDirection: 'row',
+              alignItems: 'flex-start',
+              marginTop: SIZES.padding * 2
+            }}>
+            <TouchableOpacity style={{flexDirection: 'row', alignItems: 'center'}}
+            onPress={() => console.log("phone number")}>
+                <View style={{
+                  width: 40,
+                  height: 40,
+                  backgroundColor: COLORS.lightpurple,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  borderRadius: 10,
+                }}>
+                    <Image 
+                      source={icons.phone}
+                      resizeMode = "cover"
+                      style={{
+                        height: 25,
+                        width: 25,
+                        tintColor: COLORS.purple
+                      }}
+                    />
+                </View>
+                <Text style={{
+                  marginLeft: SIZES.padding, ...FONTS.body4
+                }}>Phone Number</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              marginLeft: SIZES.padding * 2,
+            }}
+            onPress={() => console.log("Barcode")}>
+                <View style={{
+                  width: 40,
+                  height: 40,
+                  backgroundColor: COLORS.lightGreen,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  borderRadius: 10
+                }}>
+                    <Image 
+                      source={icons.barcode}
+                      resizeMode = "cover"
+                      style={{
+                        height: 25,
+                        width: 25,
+                        tintColor: COLORS.primary
+                      }}
+                    />
+                </View>
+                <Text style={{
+                  marginLeft: SIZES.padding, ...FONTS.body4
+                }}>Barcode</Text>
+            </TouchableOpacity>
+          </View>
+      </View>
+    )
+  }
+
+
+  function onBarCodeRead(result) {
+    console.log(result.data)
+  }
+
   return (
     <View style={{ flex: 1, backgroundColor: COLORS.transparent }}>
       {isFocused && ( // Render camera only if the component is focused
@@ -66,6 +230,7 @@ const Scan = () => {
             buttonNegative: 'Cancel',
           }}
           type={type}
+          onBarCodeScanned={onBarCodeRead}
           barCodeScannerSettings={{
             barCodeTypes: [BarCodeScanner.Constants.BarCodeType.qr],
           }}
@@ -73,7 +238,7 @@ const Scan = () => {
           <View
             style={{
               position: 'absolute',
-              top: 50,
+              bottom: 250,
               right: 20,
             }}
           >
@@ -93,6 +258,10 @@ const Scan = () => {
               <Text style={{ color: COLORS.black, paddingLeft: 2, }}>Flip</Text>
             </TouchableOpacity>
           </View>
+
+          {renderHeader()}
+          {renderScanFocus()}
+          {renderPaymentMethods()}
         </Camera>
       )}
     </View>
